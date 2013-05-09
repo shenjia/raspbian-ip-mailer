cdfd6905648d094ba51b9933b3e7ee067b6feab2
shenjia <zshenjia@gmail.com>
2013-05-09 16:13:48 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -2,18 +2,30 @@
 
 . /lib/lsb/init-functions
 
-log_action_msg Install ip-mailer \( by zshenjia@gmail.com \) ...
-
-SCRIPT_FILE=ip-mailer.py
+# Settings
+SCRIPT_FILE=raspbian-ip-mailer.py
 SCRIPT_PATH=/usr/local/bin
-INIT_FILE=ip-mailer
-INIT_PATH=/etc/init.d
+LAUNCHER_FILE=raspbian-ip-mailer
+LAUNCHER_PATH=/etc/init.d
 
-sudo wget https://raw.github.com/shenjia/raspbian-ip-mailer/master/ip-mailer.py -O $SCRIPT_PATH/$SCRIPT_FILE
-sudo wget https://raw.github.com/shenjia/raspbian-ip-mailer/master/ip-mailer -O $INIT_PATH/$INIT_FILE
+log_action_msg raspbian-ip-mailer \( by zshenjia@gmail.com \) ...
 
-sudo chmod +x $SCRIPT_PATH/$SCRIPT_FILE $INIT_PATH/$INIT_FILE
-sudo update-rc.d $INIT_FILE defaults
+# Download script
+log_begin_msg Downloading [ $SCRIPT_FILE ] from Github to [ $SCRIPT_PATH ]...
+sudo wget https://raw.github.com/shenjia/raspbian-ip-mailer/master/$SCRIPT_FILE -O $SCRIPT_PATH/$SCRIPT_FILE -o /dev/null
+log_end_msg $?
 
-log_success_msg Deploy ip-mailer ...
-log_action_msg Don\'t forget to setup your Gmail account in \[ $SCRIPT_PATH/$SCRIPT_FILE \]
+# Download laucher
+log_begin_msg Downloading [ $LAUNCHER_FILE ] from Github to [ $LAUNCHER_PATH ]...
+sudo wget https://raw.github.com/shenjia/raspbian-ip-mailer/master/$LAUNCHER_FILE -O $LAUNCHER_PATH/$LAUNCHER_FILE  -o /dev/null
+log_end_msg $?
+
+# Deploy
+log_begin_msg Deploying [ $LAUNCHER_FILE ] as service ...  
+sudo chmod +x $SCRIPT_PATH/$SCRIPT_FILE $LAUNCHER_PATH/$LAUNCHER_FILE
+sudo update-rc.d $LAUNCHER_FILE defaults >> /dev/null
+log_end_msg $?
+
+# Done
+log_action_msg Done! Don\'t forget to setup your Gmail account in \[ $SCRIPT_PATH/$SCRIPT_FILE \]
+
